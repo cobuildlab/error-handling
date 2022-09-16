8ec662e0be95371c0329eda915916f32a2ca6456
Angel Lacret <alacret@gmail.com>
2022-09-16 13:42:34 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,48 @@
-const quotes = ['test1', 'test2'];
+
 /**
- * Return String of the array.
- *
- * @returns String.
- * @example example
+ * @param {Function | Promise} promise - Promise function to resolve.
+ * @returns {Promise<[Error, undefined]|[null, object]>} Promise - Promise function to resolve.
  */
-export function randomQuote(): string {
-  return quotes[Math.floor(Math.random() * quotes.length)];
-}
+export async function tryCatch<T>(
+  promise: (() => Promise<T>) | Promise<T> | (() => T),
+  finallyFunction:()=>{} = null, throwIfError = false,
+): Promise<[T, undefined] | [undefined, Error]> {
+  if(typeof promise !== "function") 
+    return [undefined, new Error("")]; // Throw specific error
+  
+  return promise
+    .then((result) => {
+      return [result, null];
+    })
+    .catch((error) => {
+      if (throwIfError === true) {
+        throw error;
+      }
+      return [null, error];
+    })
+    .finally(() => {
+      if (finallyFunction !== null) {
+        if (typeof finallyFunction === 'function') {
+          finallyFunction();
+        }
+      }
+    });
+};
+
+
+/**
+ * @param {Function | Promise} promise - Promise function to resolve.
+ * @returns {Promise<[Error, undefined]|[null, object]>} Promise - Promise function to resolve.
+ */
+export async function tryCatchSync<T>(f: () => T, ...params, finallyFunction:()=>{} = null, throwIfError = false,
+): [T, undefined] | [undefined, Error]> {
+  if(typeof f !== "function") 
+    return [undefined, new Error("")]; // Throw specific error
+  
+ try {
+    const result = f(params);
+    return [result, undefined];
+  } catch (error: unknown) {
+    return [undefined, error as Error];
+  }
+};
